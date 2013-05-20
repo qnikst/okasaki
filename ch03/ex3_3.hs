bff2a9a78be8dc3a2ac9e42d18c60b31383441c6
Alexander Vershilov <alexander.vershilov@gmail.com>
2013-05-20 08:38:23 +0400

    Exercise 2.3: layout

---
+++ ch03/ex3_3.hs
@@ -0,0 +1,11 @@
+
+import Data.LeftishHeap 
+import Data.Heap.Class
+
+fromList :: Ord a => [a] -> LeftishHeap a
+fromList = head . go . (map singleton)
+  where
+    go []       = [empty]
+    go [x1]     = [x1]
+    go [x1,x2]  = [x1 `merge` x2]
+    go (x1:x2:xs) = go $ (x1 `merge` x2) : go xs
