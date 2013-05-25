66c67817605ba227bad09e426abebe5c091eb9e3
Alexander Vershilov <alexander.vershilov@gmail.com>
2013-05-25 09:53:42 +0400

    Exercise 3.7: initial layout

---
+++ ch03/ex3_7.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+import Data.Heap.Class
+
+data ExplicitMin h a = Empty | Heap a (h a) deriving (Show)
+
+instance (Heap h) => Heap (ExplicitMin h) where
+    empty = Empty
+
+    isEmpty Empty = True
+    isEmpty _ = False
+
+    insert v Empty = Heap v (v `insert` empty)
+    insert v (Heap o h) | o < v     = Heap o (v `insert` h)
+                        | otherwise = Heap v (v `insert` h)
+
+    findMin (Heap m _) = m
+    deleteMin (Heap m h) = let h' = deleteMin h 
+                           in Heap (findMin h') h'
+
+    merge Empty h = h
+    merge h Empty = h
+    merge (Heap m1 h1) (Heap m2 h2) = Heap (m1 `min` m2) (h1 `merge` h2)
