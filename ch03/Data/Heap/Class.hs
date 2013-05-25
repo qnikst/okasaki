278756eac84b9830c7095170366f9c9e3f1b0f70
Alexander Vershilov <alexander.vershilov@gmail.com>
2013-05-25 11:10:49 +0400

    Data Heap add test invariants

--- ch03/Data/Heap/Class.hs
+++ ch03/Data/Heap/Class.hs
@@ -2,6 +2,10 @@
 module Data.Heap.Class
   where
 
+import Data.List (foldl', sort)
+import Test.QuickCheck.Property
+import Test.QuickCheck.Assertions
+
 class Heap h  where
     empty     :: Ord a => h a
     isEmpty   :: Ord a => h a -> Bool
@@ -10,4 +14,25 @@ class Heap h  where
     findMin   :: Ord a => h a -> a
     deleteMin :: Ord a => h a -> h a
 
+data T a
 
+prop_min :: (Ord a, Show a, Heap h) => T (h a) -> [a] -> Bool
+prop_min t [] = True -- TODO: should fail
+prop_min t ls = go t (fromList ls) ls
+  where
+    go :: (Ord a, Show a, Heap h) => T (h a) -> h a -> [a] -> Bool
+    go _ h ls = findMin h == minimum ls
+
+prop_all :: (Ord a, Show a, Heap h) => T (h a) -> [a] -> Bool
+prop_all t [] = True -- TODO: should fail
+prop_all t ls = go t (fromList ls) ls
+  where
+    go :: (Ord a, Show a, Heap h) => T (h a) -> h a -> [a] -> Bool
+    go _ h ls = toList h == (sort ls)
+
+fromList :: (Heap h, Ord a) => [a] -> h a
+fromList = foldl' (flip insert) empty
+
+toList :: (Heap h, Ord a) => h a -> [a]
+toList h | isEmpty h = []
+         | otherwise = findMin h:toList (deleteMin h)
